e9cb29ee18fc4faafd4a68a4f658bc5e35e314e6
tesddev <tesleem.amuda@gmail.com>
2022-07-18 18:32:36 +0100

    add uibutton to view controller

--- ReusableOTPOrPinProject/ViewController.swift
+++ ReusableOTPOrPinProject/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    let child = ChildViewController()
 
+    var okButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Ok", for: .normal)
+        button.clipsToBounds = true
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        button.layer.cornerRadius = 4
+        button.layer.borderWidth = 1
+        button.backgroundColor = .systemGray
+        button.addTarget(self, action: #selector(didTapOkButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        activateConstraint()
     }
-
-
+    
+    func activateConstraint() {
+        view.addSubview(okButton)
+        NSLayoutConstraint.activate([
+            
+            okButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            okButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            okButton.widthAnchor.constraint(equalToConstant: 60),
+            okButton.heightAnchor.constraint(equalToConstant: 30),
+            
+        ])
+    }
+    
+    @objc func didTapOkButton(_ sender: Any) {
+        print("ok tapped")
+    }
 }
 
